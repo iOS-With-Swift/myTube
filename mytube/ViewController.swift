1d81c7c622160db319c53e50739cd55e52df30cd
PhongLe <phongle@PhongLes-MacBook-Pro.local>
2017-04-07 18:43:10 +0700

    Initial Commit

---
+++ mytube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mytube
+//
+//  Created by PhongLe on 4/7/17.
+//  Copyright © 2017 PhongLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
