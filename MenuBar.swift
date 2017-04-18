61b5065c85bb2fffe0742aab5f0bb62e670b25e4
Lê Hồng Phong <phongle6893@gmail.com>
2017-04-18 22:41:46 +0700

    add icon to menu bar, and show animation when click on menubar

--- MenuBar.swift
+++ MenuBar.swift
@@ -20,6 +20,7 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
     }()
     
     let cellId = "cellId"
+    let imageTop = ["home", "trending", "subscriptions", "account"]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,15 +31,21 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
         addConstrainsWithFormat(format: "H:|[v0]|", views: collectionView)
         addConstrainsWithFormat(format: "V:|[v0]|", views: collectionView)
         
+        let selectIndexPath = NSIndexPath(item: 0, section: 0)
+        collectionView.selectItem(at: selectIndexPath as IndexPath, animated: false, scrollPosition: .bottom)
+        
         backgroundColor = UIColor.makeRBG(red: 230, green: 32, blue: 31)
-     }
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MenuCell
+        
+        cell.imageView.image = UIImage(named: imageTop[indexPath.item])?.withRenderingMode(.alwaysTemplate)
+        cell.tintColor = UIColor.makeRBG(red: 91, green: 14, blue: 13)
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -54,10 +61,37 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
 }
 
 class MenuCell: BaseCell{
+    let imageView: UIImageView = {
+        ()-> UIImageView in
+        let iv = UIImageView()
+        iv.image = UIImage(named: "subscriptions")?.withRenderingMode(.alwaysTemplate)
+        iv.tintColor = UIColor.makeRBG(red: 91, green: 14, blue: 13)
+        return iv
+    }()
     
-    
+//    override var isHighlighted: Bool {
+//        didSet{
+//            imageView.tintColor = isHighlighted ? UIColor.white : UIColor.makeRBG(red: 91, green: 14, blue: 13)
+//        }
+//    }
+    override var isSelected: Bool {
+        didSet{
+            imageView.tintColor = isSelected ? UIColor.white : UIColor.makeRBG(red: 91, green: 14, blue: 13)
+        }
+    }
     override func setupView() {
         super.setupView()
-        backgroundColor = UIColor.yellow
+        
+        addSubview(imageView)
+        addConstrainsWithFormat(format: "H:[v0(28)]", views: imageView)
+        addConstrainsWithFormat(format: "V:[v0(28)]", views: imageView)
+        
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
     }
 }
+
+
+
+
+
